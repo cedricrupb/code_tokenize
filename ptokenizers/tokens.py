ad352eb2f3ce527b189c1153c2984318e7b406fe
Cedric Richter <cedricr@mail.upb.de>
2021-10-30 19:32:16 +0200

    Support for statement head + parent statement head

--- ptokenizers/tokens.py
+++ ptokenizers/tokens.py
@@ -6,9 +6,9 @@ from .parsers import match_span
 
 class Token:
 
-    def __init__(self, config, node, source_lines):
+    def __init__(self, config, ast_node, source_lines):
         self.config = config
-        self.node = node
+        self.ast_node = ast_node
         self.source_lines = source_lines
         self.root_sequence = None
 
@@ -36,24 +36,19 @@ class Token:
 
     @cached_property
     def text(self):
-        return match_span(self.node, self.source_lines)
+        return match_span(self.ast_node, self.source_lines)
 
     @property
     def type(self):
-        return self.node.type
+        return self.ast_node.type
 
     @cached_property
     def statement_head(self):
         """Returns the token representing the head of a statement"""
 
         statement_types = self.config.statement_types
-        def is_statement(type):
-            return any(match_type(reg, type) for reg in statement_types)
         
-        # Go up till we find a statement node
-        parent_node = self.node.parent
-        while parent_node is not None and not is_statement(parent_node.type):
-            parent_node = parent_node.parent
+        parent_node = parent_statement_node(statement_types, self.ast_node)
         if parent_node is None: raise ValueError("No statement could be identified!")
 
         # Identify first token that belongs to the statement
@@ -62,6 +57,31 @@ class Token:
             current_left = current_left.children[0]
 
         return self._create_token(current_left)
+
+    @cached_property
+    def parent_head(self):
+        """
+        Returns head of parent node if it exists.
+
+        If the current token belongs to a top level statement,
+        the function return None.
+        """
+        # For identifying statements
+        statement_types = self.config.statement_types
+        parent_node     = parent_statement_node(statement_types, self.ast_node)
+        if parent_node is None: raise ValueError("No statement could be identified!")
+
+        grandparent_node = parent_statement_node(statement_types, parent_node)
+        if grandparent_node is None: return None
+
+        # Identify first token that belongs to the statement
+        current_left = grandparent_node
+        while not is_token(current_left):
+            current_left = current_left.children[0]
+
+        return self._create_token(current_left)
+
+
 
 
 # Token Collection -----------------------------------------------------
@@ -75,7 +95,7 @@ class TokenSequence(list):
 
         for tok in self:
             tok.root_sequence = self
-            self._map_nodes[node_key(tok.node)] = tok
+            self._map_nodes[node_key(tok.ast_node)] = tok
 
     def get_token_by_node(self, node):
         return self._map_nodes[node_key(node)]
@@ -118,13 +138,23 @@ def match_type(type_regex, type):
 
     raise ValueError("Unsupported type regex: %s" % type_regex)
 
+
 def is_token(node):
     return node.type == "string" or not node.children 
+
 
 def node_key(node):
     return (node.type, node.start_point, node.end_point)
 
 
+def parent_statement_node(statement_types, node):
 
+    def is_statement(type):
+        return any(match_type(reg, type) for reg in statement_types)
+        
+    # Go up till we find a statement node
+    parent_node = node.parent
+    while parent_node is not None and not is_statement(parent_node.type):
+        parent_node = parent_node.parent
 
-
+    return parent_node
