607bbcafecf489996a19dcc3ceee9526a6720176
Cedric Richter <cedricr@mail.upb.de>
2021-11-01 16:24:01 +0100

    Guessing the parser cannot easily be solved. TODO: Guess lang by bayesian spam filter

--- code_tokenize/__init__.py
+++ code_tokenize/__init__.py
@@ -3,9 +3,16 @@ from .parsers import ASTParser
 from .config  import TokenizationConfig
 from .tokenizer import tokenize_tree
 
+import logging as logger
+
 # Main function --------------------------------
 
-def tokenize(source_code, lang = "python", **kwargs):
+def tokenize(source_code, lang = "guess", **kwargs):
+
+    # If lang == guess, automatically determine the language
+    if lang == "guess": lang = lang_detect(source_code)
+
+    logger.debug("Parses source code with parser for %s" % lang)
 
     # Setup config
     config = TokenizationConfig(lang, **kwargs)
@@ -18,4 +25,12 @@ def tokenize(source_code, lang = "python", **kwargs):
 
 
 
-# -----------------------------------------------
+# Lang detect --------------------------------------  
+
+
+def lang_detect(source_code):
+    """Guesses the source code type using pygments"""
+    raise NotImplementedError(
+        "Guessing the language automatically is currently not implemented. Please specify a language with the lang keyword\n code_tokenize.tokenize(code, lang = your_lang)"
+    )
+
