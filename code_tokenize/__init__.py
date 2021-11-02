3a2901a7391dce6c18eccfbd6dfba3cc6d8672aa
Cedric Richter <cedricr@mail.upb.de>
2021-11-02 16:11:58 +0100

    Allow to load the tokenization config from a language specific config file

--- code_tokenize/__init__.py
+++ code_tokenize/__init__.py
@@ -1,6 +1,6 @@
 
 from .parsers import ASTParser
-from .config  import TokenizationConfig
+from .config  import load_from_lang_config
 from .tokenizer import tokenize_tree
 
 import logging as logger
@@ -9,18 +9,20 @@ import logging as logger
 
 def tokenize(source_code, lang = "guess", **kwargs):
 
+    if len(source_code.strip()) == 0: raise ValueError("The code string is empty. Cannot tokenize anything empty: %s" % source_code) 
+
     # If lang == guess, automatically determine the language
     if lang == "guess": lang = lang_detect(source_code)
 
     logger.debug("Parses source code with parser for %s" % lang)
 
     # Setup config
-    config = TokenizationConfig(lang, **kwargs)
+    config = load_from_lang_config(lang, **kwargs)
 
     # Parse source tree
     parser = ASTParser(config.lang)
     tree, code = parser.parse(source_code)
-
+    
     return tokenize_tree(config, tree.root_node, code)
 
 
--- code_tokenize/config.py
+++ code_tokenize/config.py
@@ -1,20 +1,72 @@
+import os
+import json
+
 
 class TokenizationConfig:
 
     def __init__(self, lang, **kwargs):
         self.lang = lang
+        self.syntax_error = "raise" # Options: raise, warn, ignore
+
+        self.ident_tokens = False # Whether to represent indentations and newlines (Helpful for script languages like Python)
 
         # A list of all statement node defined in the language
         self.statement_types = [
             "*_statement", "*_definition", "*_declaration"
         ]
 
+        self.update(kwargs)
+
     
     def update(self, kwargs):
-        for k, v in kwargs.iteritems():
+        for k, v in kwargs.items():
 
             if k not in self.__dict__:
                 raise TypeError("TypeError: tokenize() got an unexpected keyword argument '%s'" % k)
         
             self.__dict__[k] = v
+    
+    def __repr__(self):
+
+        elements = []
+        for k, v in self.__dict__.items():
+            if v is not None:
+                elements.append("%s=%s" % (k, v))
+        
+        return "Config(%s)" % ", ".join(elements)
+
+
+
+# From config ----------------------------------------------------------------
+
+def load_from_config(config_path, **kwargs):
+    """Load from a config file. Config options can still be overwritten with kwargs"""
+
+    with open(config_path, "r") as config_file:
+        config = json.load(config_file)
+    config.update(kwargs)
+
+    return TokenizationConfig(**config)
+
+
+def _get_config_path():
+    current_path = os.path.abspath(__file__)
+
+    while len(current_path) > 0 and os.path.basename(current_path) != "code_tokenize":
+        current_path = os.path.dirname(current_path)
+    parent_path = os.path.dirname(current_path)
+
+    return os.path.join(parent_path, "lang_configs")
+
+
+def load_from_lang_config(lang, **kwargs):
+    config_path = _get_config_path()
+    config_path = os.path.join(config_path, "%s.json" % lang)
+
+    if os.path.exists(config_path):
+        kwargs["lang"] = lang
+        return load_from_config(config_path, **kwargs)
+
+    return TokenizationConfig(lang, **kwargs)
+
 
