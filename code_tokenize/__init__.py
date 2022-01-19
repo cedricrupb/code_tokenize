440178c83abf18f77379053ea2394300f8aa9e72
Cedric Richter <cedricr@mail.upb.de>
2022-01-19 17:29:35 +0100

    Add documentation to tokenize function

--- code_tokenize/__init__.py
+++ code_tokenize/__init__.py
@@ -8,11 +8,47 @@ import logging as logger
 # Main function --------------------------------
 
 def tokenize(source_code, lang = "guess", **kwargs):
+    """
+    Tokenizes source code of most programming languages quickly.
+
+    Given source code as string, this function quickly tokenizes
+    the code into basic program tokens. 
+    The function uses tree-sitter as a backend. Therefore, this
+    function does not only support most programming languages (see README)
+    but also relates every token to an AST node.
+    Tokens can be abused to traverse the program AST.
+
+    Parameters
+    ----------
+    source_code : str
+        Source code to parsed as a string. Also
+        supports parsing of incomplete source code
+        snippets (by deactivating the syntax checker; see syntax_error)
+    
+    lang : [python, java, javascript, ...]
+        String identifier of the programming language
+        to be parsed. Supported are most programming languages
+        including python, java and javascript (see README)
+        Default: guess (Guesses language / Not supported currently throws error currently)
+    
+    syntax_error : [raise, warn, ignore]
+        Reaction to syntax error in code snippet.
+        raise:  raises a Syntax Error
+        warn:   prints a warning to console
+        ignore: Ignores syntax errors. Helpful for parsing code snippets.
+        Default: raise
+
+    Returns
+    -------
+    TokenSequence
+        A list of tokens representing the source code snippet.
+    
+    """
 
     if len(source_code.strip()) == 0: raise ValueError("The code string is empty. Cannot tokenize anything empty: %s" % source_code) 
 
     # If lang == guess, automatically determine the language
-    if lang == "guess": lang = lang_detect(source_code)
+    if lang == "guess": lang = _lang_detect(source_code)
 
     logger.debug("Parses source code with parser for %s" % lang)
 
@@ -30,7 +66,7 @@ def tokenize(source_code, lang = "guess", **kwargs):
 # Lang detect --------------------------------------  
 
 
-def lang_detect(source_code):
+def _lang_detect(source_code):
     """Guesses the source code type using pygments"""
     raise NotImplementedError(
         "Guessing the language automatically is currently not implemented. Please specify a language with the lang keyword\n code_tokenize.tokenize(code, lang = your_lang)"
